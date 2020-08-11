4abd580747277f455eb5cc959f9ed95b096dfc01
Zerio <43386412+ZerioDev@users.noreply.github.com>
2020-08-11 23:57:49 +0200

    🎉 Logs with Webhook

--- bot.js
+++ bot.js
@@ -8,3 +8,26 @@ const client = new Discord.Client();
 client.on("ready", () => {
   console.log("Launched bot !")
 });
+
+//New Webhook
+client.statusHook = new Discord.WebhookClient("ID", "PASSWORD")
+
+//Shard ready
+client.on("shardReady", async shard => {
+    client.statusHook.send(`Shard **#${shard}** ready on **${client.guilds.cache.size}** servers and **${client.users.cache.size}** users.`)
+})
+
+//Shard disconnect
+client.on("shardDisconnect", async shard => {
+    client.statusHook.send(`Shard **#${shard}** disconnected from its servers and users temporarily...`)
+})
+
+//Shard reconnecting
+client.on("shardReconnecting", async shard => {
+    client.statusHook.send(`Shard **#${shard}** reconnection in progress on the servers containing this shard...`)
+})
+
+//Shard resume
+client.on("shardResume", async shard => {
+    client.statusHook.send(`Shard **#${shard}** successfully reconnected to **${client.guilds.cache.size}** servers and **${client.users.cache.size}** users.`)
+})
